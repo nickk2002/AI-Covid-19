3304bab94b7db139a1d0439b201d2d5356e6e1cb
TheNicMic <nicmic8352@gmail.com>
2020-03-24 22:48:50 +0200

    miscare NavMesh
    Da

---
+++ AI Covid 19/Assets/Scripts/MovementIntrePuncte.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.AI;
+using UnityEngine;
+
+public class MovementIntrePuncte : MonoBehaviour
+{
+    public GameObject[] pozitii = new GameObject[3];
+    public int current = 0;
+    float radius = 2f;
+    public bool moving = false;
+
+    NavMeshAgent agent;
+    Vector3 destinatie;
+    void Start()
+    {
+        
+        agent = GetComponent<NavMeshAgent>();
+
+        for (int i=0; i <= 2; i++)
+        {
+
+            pozitii[i] = GameObject.Find("Loc" + i);
+
+
+            
+
+        }
+
+        destinatie = agent.destination;
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if(moving == false)
+        {
+            destinatie = pozitii[current].transform.position;
+            agent.destination = destinatie;
+            moving = true;
+        }
+
+            if (Vector3.Distance(pozitii[current].transform.position, transform.position) < radius)
+            {
+
+            moving = false;
+
+                current++;
+                if (current >= pozitii.Length)
+                {
+                    current = 0;
+                    
+                }
+
+
+            }
+
+            
+
+        
+        
+    }
+}
