33b7966d32a866ef3da52e910e191aea48a3d7eb
nickk2002 <nicolae.filat@yahoo.com>
2020-04-05 11:58:36 +0300

    AI Simulation Scene

---
+++ AI Covid 19/Assets/Scripts/BotSpawner.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BotSpawner : MonoBehaviour
+{
+    public GameObject enemy;
+    public int numar;
+
+
+    void Pune(int index)
+    {
+        Vector3 pos = new Vector3(Random.Range(-500f, 500f), 0, Random.Range(-500f, 500f));
+        GameObject bot = Instantiate(enemy,pos,Quaternion.identity);
+        bot.transform.SetParent(transform);
+        if (index == 3 || index == 7)
+            bot.GetComponent<Bot>().infected = true;
+        //Debug.Log("I am here bot spwa");
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        for (int i = 1; i <= numar; i++)
+        {
+            Pune(i);
+        }
+    }
+
+}
