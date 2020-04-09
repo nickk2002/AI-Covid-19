7c50f719f49d2f0f29fc3111da1496b78cef1be4
nickk2002 <nicolae.filat@yahoo.com>
2020-04-09 10:33:52 +0300

    bot.cs + gamemanager.cs some english comments + elevator
    I am tired for all the comments. I will do them a bit later.
    Play the scene SimulareAI.unity (and change the values for bot1)
    for ex : change infectDistance to 30

---
+++ AI Covid 19/Assets/Scripts/Elevator.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Elevator : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    public void MoveUp()
+    {
+        GetComponent<Animator>().SetBool("move", true);
+    }
+    public void MoveDown()
+    {
+        GetComponent<Animator>().SetBool("move", true);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
