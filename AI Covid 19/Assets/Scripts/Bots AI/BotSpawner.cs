c088396a4df3d6fae395b565b0625b67580ed300
nickk2002 <nicolae.filat@yahoo.com>
2020-04-10 09:53:01 +0300

    laboratory + bot.cs
    the bots go to the hospital and get cured

--- AI Covid 19/Assets/Scripts/Bots AI/BotSpawner.cs
+++ AI Covid 19/Assets/Scripts/Bots AI/BotSpawner.cs
@@ -1,17 +1,33 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class BotSpawner : MonoBehaviour
 {
     public GameObject enemy;
-    public float randomPosRange = 500;
+    public float randomRange = 500;
     public int howMany;
 
+    Vector3 RandomPos()
+    {
+        Collider[] colliders;
+        Vector3 position;
+        int cnt = 0;
+        do
+        {
+            position = new Vector3(Random.Range(-randomRange, randomRange), 0, Random.Range(-randomRange, randomRange));
+            colliders = Physics.OverlapSphere(position, 5f);
+            cnt++;
+            if (cnt > 100)
+                Debug.Break();
+        } while (colliders.Length > 1);
 
+        return position;
+    }
     void Pune(int index)
     {
-        Vector3 pos = new Vector3(Random.Range(-randomPosRange, randomPosRange), 0, Random.Range(-randomPosRange, randomPosRange));
+        Vector3 pos = RandomPos();
         GameObject bot = Instantiate(enemy,pos,Quaternion.identity);
         bot.transform.SetParent(transform);
         if (index == 1 || index == 2 || index == 3)// start with 3 already infected Bots for the simulation
