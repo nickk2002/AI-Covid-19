eff7ff5a68505475fb98a0d3d9a2de0d1a36e925
nickk2002 <nicolae.filat@yahoo.com>
2020-04-20 16:44:56 +0300

    Blend tree animations
    Former-commit-id: e6e7381aa9cb4cea0eedc1309f4904a38d8a670c

---
+++ AI Covid 19/Assets/_Scripts/AI Related/AnimateAI.cs
@@ -0,0 +1,121 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class AnimateAI : MonoBehaviour
+{
+    [SerializeField] float speedLowAngle;
+    [SerializeField] float speedHighAngle;
+    [SerializeField] private float angleAccuracy = 1f;
+    NavMeshAgent agent;
+    Animator animator;
+    Vector3 lastDirection;
+    
+    float angle;
+    float lastAngle;
+    private bool rotating = false;
+    private bool lastBool = false;
+    private bool smallAngleRot = false;
+
+
+    IEnumerator Rotate()
+    {
+        smallAngleRot = true;
+        Quaternion initialRot = transform.rotation;
+        Quaternion desiredRot = transform.rotation * Quaternion.Euler(0, angle, 0);
+        float initialTime = Time.time;
+        float duration = 0.25f;
+        
+        while (Time.time - initialTime > duration)
+        {
+            transform.rotation = Quaternion.Lerp(initialRot, desiredRot, (Time.time - initialTime) / duration);
+            yield return null;
+        }
+        transform.rotation = desiredRot;
+        smallAngleRot = false;
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+        agent = GetComponent<NavMeshAgent>();
+        agent.updatePosition = false;
+        agent.updateRotation = false;
+        
+    }
+    void Move(Vector3 direction)
+    {
+        if (agent.hasPath == false)
+            return;
+        angle = Vector3.Angle(transform.forward, direction) * Mathf.Sign(Vector3.Dot(transform.right, direction));
+        //angle = Mathf.Atan(direction.x / direction.z) * Mathf.Rad2Deg;
+        
+        /// daca am 30-60 de grade vreau rapid. adica angle * 10
+        /// daca am 90-180 de grade vreau ma incet adica angle * 7
+        float multiply;
+        if (angle > 90)
+             multiply = speedHighAngle;
+        else
+            multiply = speedLowAngle;
+
+        if (Mathf.Abs(lastAngle) < angleAccuracy && angle != 0)
+        {
+            animator.SetFloat("Rotation",angle / 90);
+        }
+        animator.SetFloat("Velocity",direction.magnitude / agent.speed);
+        
+        rotating = (Mathf.Abs(angle) > angleAccuracy);
+        lastAngle = angle;
+    }
+
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (agent.isStopped == false)
+            lastDirection = agent.desiredVelocity;
+        Move(lastDirection);
+    }
+    private void OnAnimatorMove()
+    {
+        transform.rotation = animator.rootRotation;
+        if (rotating)
+        {
+            agent.isStopped = true;
+            transform.position = animator.rootPosition;
+            agent.nextPosition = transform.position;
+            Debug.Log("now rotating");
+        }
+        else
+        {
+            Debug.Log("now moving");
+            //if(Vector3.Distance(transform.position,agent.destination) > 1f)
+              //  transform.forward = lastDirection;
+            agent.isStopped = false;
+            transform.position = agent.nextPosition;
+        }
+
+    }
+    private void OnDrawGizmos()
+    {
+        if (agent != null)
+        {
+            Gizmos.color = Color.black;
+            Gizmos.DrawWireSphere(agent.nextPosition, 0.3f);
+            Gizmos.DrawLine(transform.position, transform.position + transform.forward * 3);
+            Gizmos.color = Color.magenta;
+            Vector3 velocity = lastDirection;
+            Gizmos.DrawLine(agent.nextPosition, agent.nextPosition + velocity);
+            
+            if (agent.destination != null)
+            {
+                Gizmos.color = Color.cyan;
+                Gizmos.DrawSphere(agent.destination, 0.25f);
+            }
+        }
+        
+    }
+
+}
---
+++ AI Covid 19/Assets/_Scripts/Player Actions/Player.cs
@@ -0,0 +1,113 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+using TMPro;
+using UnityEngine.UI;
+
+public class Player : MonoBehaviour
+{
+    // Start is called before the first frame update
+    static public Player Instance;
+    public bool hasObject = false;
+    public AudioClip [] coughSoundArray;
+    [Header("Coughing")]
+    public float coughInfectDistance = 5f;
+    public Image coughLoadingBarUI;
+    public TextMeshProUGUI coughTextUI;
+
+
+    float currentTimer;
+    AudioClip coughClip;
+    AudioClip lastAudioClip;
+    AudioSource source;
+
+
+    void Awake()
+    {
+        if (Instance == null)
+            Instance = this;
+        source = GetComponent<AudioSource>();
+        
+    }
+    private void Start()
+    {
+        
+    }
+
+    AudioClip RandomAudio()
+    {
+        AudioClip clip = coughSoundArray[Random.Range(0, coughSoundArray.Length - 1)];
+        int tries = 0;
+        while(clip == lastAudioClip && tries <= 3)
+        {
+            clip = coughSoundArray[Random.Range(0, coughSoundArray.Length - 1)];
+            tries++;
+        }
+
+        lastAudioClip = clip;
+        return clip;
+    }
+    bool CanInfect(Bot bot)
+    {
+        float dist = Vector3.Distance(transform.position, bot.transform.position);
+        if (dist <= coughInfectDistance && bot.cured == false)
+            return true;
+        return false;
+    }
+    void TryInfectSomeone()
+    {
+        foreach(Bot bot in GameManager.instance.listBots)
+        {
+            /// daca nu este infectat si este in distanta potrivita atunci il infecteaza
+            if (bot.infectionLevel == 0 && CanInfect(bot))
+            {
+                //Debug.Log("Player has Infected bot : " + bot.name);
+                bot.StartInfection();
+                return;
+            }
+        }
+    }
+    void CoughMecanic()
+    {
+        if (Input.GetKeyDown(KeyCode.C) && (coughClip == null || currentTimer > coughClip.length))
+        {
+            coughClip = RandomAudio();
+            //Debug.Log("Playing clip : " + coughClip.name);
+            source.PlayOneShot(coughClip);
+            TryInfectSomeone();
+            currentTimer = 0;
+        }
+        else if (coughClip != null)
+        {
+            if (currentTimer < coughClip.length)
+            {
+                if (coughTextUI != null)
+                {
+                    int value = (int)(currentTimer / coughClip.length * 100);
+                    coughTextUI.text = value.ToString() + "%";
+                }
+                if (coughLoadingBarUI)
+                    coughLoadingBarUI.fillAmount = currentTimer / coughClip.length;
+                currentTimer += Time.deltaTime;
+            }
+            else
+            {
+                if (coughTextUI != null)
+                    coughTextUI.text = "100%";
+                if (coughLoadingBarUI)
+                    coughLoadingBarUI.fillAmount = 1;
+            }
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        CoughMecanic();
+    }
+    private void OnDrawGizmos()
+    {
+        
+    }
+}
