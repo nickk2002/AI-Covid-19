7345a02c9c1b08dacf0be381ea8d636a12daa935
nickk2002 <nicolae.filat@yahoo.com>
2020-04-26 15:27:19 +0300

    Add Bot View Panel
    Former-commit-id: 333e4a519e2029a27974de91bda7cd2e44cfe494

---
+++ AI Covid 19/Assets/_Scripts/UI/UIManager.cs
@@ -0,0 +1,29 @@
+﻿using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIManager : MonoBehaviour
+{
+    public static UIManager Instance;
+    public Canvas canvas;
+    public Text textInfectPercentage;
+    public TextMeshProUGUI coughText;
+    public Image coughLoadingBarUI;
+    
+    // Start is called before the first frame update
+    void Awake()
+    {
+        if (Instance == null)
+            Instance = this;
+        if (canvas == null)
+        {
+            Debug.LogError("Canvas not set in UI Manager");
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
