4de2cdf307cf83ca6d806388d35d6906ecfbceab
nickk2002 <nicolae.filat@yahoo.com>
2020-05-19 17:47:37 +0300

    modificari totale + scena
    Former-commit-id: cdc17fe68a012f87d6d7dd13c3aa97e9563d75c5

---
+++ AI Covid 19/Assets/Editor/ShowOnlyAttribute.cs
@@ -0,0 +1,13 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+[CustomPropertyDrawer(typeof(ShowOnlyAttribute))]
+public class ShowOnlyDrawer : PropertyDrawer
+{
+    public override void OnGUI(Rect position, SerializedProperty prop, GUIContent label)
+    {
+        GUI.enabled = false;
+        EditorGUI.PropertyField(position, prop, label);
+        GUI.enabled = true;
+    }
+}
