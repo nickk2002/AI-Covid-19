bfce2ef8b726e157cf4fd73cf168883eff2de001
nickk2002 <nicolae.filat@yahoo.com>
2020-05-19 17:47:37 +0300

    modificari totale + scena
    Former-commit-id: cdc17fe68a012f87d6d7dd13c3aa97e9563d75c5

---
+++ AI Covid 19/Assets/ANA.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "Data", menuName = "Numele meu", order = 1)]
+public class ANA : ScriptableObject
+{
+    public Vector3 ceva;
+    public float myflaot;
+    public int numar;
+
+
+}
