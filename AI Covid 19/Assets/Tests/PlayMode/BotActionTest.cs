d3232f2e5f8ade5dab0a0ae9e55a2d8e31adb6b0
nickk2002 <nicolae.filat@yahoo.com>
2020-05-25 16:58:47 +0300

    Created proper editor tests for action editor
    Former-commit-id: 58b6223e340a50146979e8b5492a36f8a790d1ef

---
+++ AI Covid 19/Assets/Tests/PlayMode/BotActionTest.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using NUnit.Framework;
+using UnityEngine;
+using System.Linq;
+using UnityEngine.TestTools;
+using UnityEngine.SceneManagement;
+
+namespace Tests
+{
+    public class BotActionTest
+    {
+        [UnityTest]
+        public IEnumerator CheckIfVarsNotResetAfterPlayMode()
+        {
+            yield return new WaitForSeconds(0.25f);
+            SceneManager.LoadScene("BasicScene");
+            yield return new WaitForSeconds(0.25f);
+            Bot randomBot = UnityEngine.Object.FindObjectsOfType<Bot>()[0];
+            //store the data
+            string jsonContent = SaveSystem.GetJsonStringForBotActions();
+            randomBot.washHandsAction.position.x += 0.5f;
+            randomBot.SavePreset();
+            randomBot.UpdatePreset();
+            
+            foreach (Bot otherBot in UnityEngine.Object.FindObjectsOfType<Bot>())
+            {
+                Debug.Assert(otherBot.reflectionActions.SequenceEqual(randomBot.reflectionActions));
+            }
+            SaveSystem.WriteIntroJsonForBotActions(jsonContent);
+        }
+    }
+}
---
+++ AI Covid 19/Assets/_Scripts/AI Related/Actions/SaveSystem.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using System.Text.RegularExpressions;
+using UnityEngine;
+//using System.Runtime.Serialization.Formatters.Binary.Formatters.Binary;
+
+public class SaveSystem
+{
+
+    [System.Serializable]
+    class ActionsKeeper
+    {
+        public List<BotActionData> actionDataList;
+        public ActionsKeeper()
+        {
+
+        }
+        public ActionsKeeper(List<BotActionData> actionData)
+        {
+            actionDataList = actionData;
+        }
+    }
+    static public string GetJsonStringForBotActions()
+    {
+        string jsonPath = Application.persistentDataPath + "/savedActions.txt";
+        string content = File.ReadAllText(jsonPath);
+        return content;
+    }
+    static public void WriteIntroJsonForBotActions(string content)
+    {
+        string jsonPath = Application.persistentDataPath + "/savedActions.txt";
+        File.WriteAllText(jsonPath, content);
+    }
+    static private List<BotActionData> ConvertFromBotActionToData(List<BotAction> actionList)
+    {
+        List<BotActionData> dataList = new List<BotActionData>();
+        foreach (BotAction action in actionList)
+            dataList.Add(new BotActionData(action));
+        return dataList;
+    }
+    static private List<BotAction> ConvertFromDataToBotAction(List<BotActionData> actionDataList)
+    {
+        List<BotAction> actionList = new List<BotAction>();
+        foreach (BotActionData actionData in actionDataList)
+            actionList.Add(actionData.ConvertToBotAction());
+        return actionList;
+    }
+    static public void SaveBotAsJson(List<BotAction>actionList)
+    {
+        List<BotActionData> actionDataList = ConvertFromBotActionToData(actionList);
+        ActionsKeeper actionKeeper = new ActionsKeeper(actionDataList);
+        string jsonMessage = JsonUtility.ToJson(actionKeeper, true);
+        Debug.Log(jsonMessage);
+        string jsonPath = Application.persistentDataPath + "/savedActions.txt";
+
+        File.WriteAllText(jsonPath, jsonMessage);
+        Debug.Log("saved json at " + jsonPath);
+    }
+    static public List<BotAction> LoadBotActions()
+    {
+        string jsonPath = Application.persistentDataPath + "/savedActions.txt";
+        string jsonMessage = File.ReadAllText(jsonPath);
+        if (File.Exists(jsonPath)){
+            ActionsKeeper actionKeeper = JsonUtility.FromJson<ActionsKeeper>(jsonMessage);
+            return ConvertFromDataToBotAction(actionKeeper.actionDataList);
+        }
+        else
+        {
+            Debug.LogError("Nu exista jsonPath " + jsonPath);
+            return null;
+        }
+    }
+}
