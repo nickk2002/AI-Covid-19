60c75633d6a8a4b3c65257f9a3916984813aaf2d
nickk2002 <nicolae.filat@yahoo.com>
2020-05-25 16:58:47 +0300

    Created proper editor tests for action editor
    Former-commit-id: 58b6223e340a50146979e8b5492a36f8a790d1ef

---
+++ AI Covid 19/Assets/_Scripts/MeetPoint.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+public class MeetPoint : MonoBehaviour
+{
+    static public List<MeetPoint> list = new List<MeetPoint>();
+    public bool occupied;
+
+    public static void ClearList()
+    {
+        list.Clear();
+    }
+    private void Start()
+    {
+        list.Add(this);
+    }
+
+
+}
