763893e86c66a45f5c61a0965fc762f8de5c430a
nickk2002 <nicolae.filat@yahoo.com>
2020-06-05 21:34:53 +0300

    Adjusted namespaces and nice folders
    Former-commit-id: acbe224b9aeda8478e96e0e463b711b988bf9864

--- AI Covid 19/Assets/Tests/PlayMode/BotActionTest.cs
+++ AI Covid 19/Assets/Tests/PlayMode/BotActionTest.cs
@@ -1,10 +1,10 @@
-﻿using Covid19.SaveData;
-using System.Collections;
+﻿using System.Collections;
 using System.Linq;
+using Covid19.AIBehaviour;
+using Covid19.AIBehaviour.SaveData;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.TestTools;
-using Covid19.AIBehaviour;
 
 namespace Tests
 {
@@ -16,14 +16,14 @@ namespace Tests
             yield return new WaitForSeconds(0.25f);
             SceneManager.LoadScene("BasicScene");
             yield return new WaitForSeconds(0.25f);
-            Bot randomBot = UnityEngine.Object.FindObjectsOfType<Bot>()[0];
+            Bot randomBot = Object.FindObjectsOfType<Bot>()[0];
             //store the data
             string jsonContent = SaveSystem.GetJsonStringForBotActions();
-            randomBot._washHandsAction.position.x += 0.5f;
+            randomBot.washHandsAction.position.x += 0.5f;
             randomBot.SavePreset();
             randomBot.UpdatePreset();
 
-            foreach (Bot otherBot in UnityEngine.Object.FindObjectsOfType<Bot>())
+            foreach (Bot otherBot in Object.FindObjectsOfType<Bot>())
             {
                 Debug.Assert(otherBot.reflectionActions.SequenceEqual(randomBot.reflectionActions));
             }
