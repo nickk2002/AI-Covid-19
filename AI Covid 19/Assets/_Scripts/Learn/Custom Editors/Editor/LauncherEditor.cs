39d9191e0fb0130f6d394b8b64d889f6720be523
nickk2002 <nicolae.filat@yahoo.com>
2020-06-11 11:18:27 +0300

    Added code refactorings + Quest Select for player
    Added namespaces, fixed name conventions using rider
    _privateVariable for private fields
    
    
    Former-commit-id: 005a78ad60a5eb91d6bc28d3c9c3c5222f76d8e2

---
+++ AI Covid 19/Assets/_Scripts/Learn/Custom Editors/Editor/LauncherEditor.cs
@@ -0,0 +1,31 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+namespace Covid19.Learn.Custom_Editors.Editor
+{
+    [CustomEditor(typeof(Launcher))]
+    public class LauncherEditor : UnityEditor.Editor
+    {
+        private void OnSceneGUI()
+        {
+            Launcher launcher = target as Launcher;
+            Transform transform = launcher.transform;
+            launcher.offset = Handles.PositionHandle(transform.position + launcher.offset, Quaternion.identity) - transform.position;
+            Handles.Label(transform.position + launcher.offset, "Offset");
+            Handles.BeginGUI();
+            Vector3 position = Camera.current.WorldToScreenPoint(launcher.transform.position + launcher.offset);
+            var rect = new Rect();
+            rect.x = position.x;
+            rect.yMin = SceneView.currentDrawingSceneView.position.height - position.y;
+            rect.width = 64;
+            rect.height = 16;
+            GUILayout.BeginArea(rect);
+            if (GUILayout.Button("Fire"))
+                launcher.Fire();
+            GUILayout.EndArea();
+            Handles.EndGUI();
+
+            //launcher.offset = transform.InverseTransformPoint(Handles.PositionHandle(transform.TransformPoint(launcher.offset), transform.rotation));
+        }
+    }
+}
