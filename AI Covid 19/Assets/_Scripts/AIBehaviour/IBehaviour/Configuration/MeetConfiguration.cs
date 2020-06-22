0c08814fcdad4761816b22c2b897d7583e6c5b11
nickk2002 <nicolae.filat@yahoo.com>
2020-06-22 16:15:33 +0300

    Added scriptable objects for configuration
    So currently, there are PatrolConfiguration and MeetConfiguration. One issue is that the position holder can't be included in the patrol configuration unless is a prefab in assets. I will keep it as it is now

---
+++ AI Covid 19/Assets/_Scripts/AIBehaviour/IBehaviour/Configuration/MeetConfiguration.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Covid19.AIBehaviour.Behaviour.States
+{
+    [CreateAssetMenu(menuName = "AI Behaviour/Meet")]
+    public class MeetConfiguration : ScriptableObject
+    {
+        [Range(1,10)] public int sociabalLevel;
+        [Range(1,5)] public int offsetMeeting;
+        public float viewDist;
+        public float viewAngle;
+        public float cooldownMeeting;
+    }
+}
