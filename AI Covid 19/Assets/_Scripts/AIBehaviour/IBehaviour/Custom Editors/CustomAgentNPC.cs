b4fd396b0a3ccbde4500aee7b1f033ef69b2c592
nickk2002 <nicolae.filat@yahoo.com>
2020-06-22 17:09:00 +0300

    Refacted the folder names and also added a preview for scriptable object

---
+++ AI Covid 19/Assets/_Scripts/AIBehaviour/IBehaviour/Custom Editors/CustomAgentNPC.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using Covid19.AIBehaviour.Behaviour;
+using UnityEditor;
+using UnityEngine;
+
+[CustomEditor(typeof(AgentNPC))]
+public class CustomAgentNPC : Editor
+{
+    private Editor _cachedEditor;
+
+    private void OnEnable()
+    {
+        _cachedEditor = null;
+    }
+
+    public override void OnInspectorGUI()
+    {
+        AgentNPC agentNPC = target as AgentNPC;
+        if (_cachedEditor == null)
+            _cachedEditor = Editor.CreateEditor(agentNPC.meetConfiguration);
+        base.OnInspectorGUI();
+        
+        _cachedEditor.DrawDefaultInspector();
+        
+    }
+}
