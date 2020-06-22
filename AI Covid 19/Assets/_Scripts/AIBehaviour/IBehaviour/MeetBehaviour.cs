fa12f9bd5a372fc69b11d2b52ed081be2f1be318
nickk2002 <nicolae.filat@yahoo.com>
2020-06-22 13:58:12 +0300

    Added MeetingSystem.cs and Also the meeting works in combination with typing and patroling. Added a dictionary in the agent to manage the coroutines.

---
+++ AI Covid 19/Assets/_Scripts/AIBehaviour/IBehaviour/MeetBehaviour.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using UnityEngine;
+
+namespace Covid19.AIBehaviour.Behaviour
+{
+    public class MeetBehaviour : MonoBehaviour, IBehaviour
+    {
+        public Vector3 MeetPosition { set; private get; }
+        private Animator _animator;
+        private AgentNPC _npc;
+
+        private static readonly int Talking = Animator.StringToHash("talking");
+
+        public void Enable()
+        {
+            Debug.Log("Am intrat in enable de la meeting");
+            _npc = GetComponent<AgentNPC>();
+            _npc.Agent.SetDestination(MeetPosition);
+            _animator = GetComponent<Animator>();
+        }
+
+
+        public void Disable()
+        {
+
+        }
+
+        public IEnumerator OnUpdate()
+        {
+            while (true)
+            {
+                Debug.Assert(_npc.Agent != null,"Assertion failed");
+                if (_npc.Agent.remainingDistance < 0.1f)
+                {
+                    _npc.Agent.isStopped = true;
+                    _animator.SetBool(Talking, true);
+                    yield return new WaitForSeconds(UnityEngine.Random.Range(3,5));
+                    Debug.Log("Finished the talking stuff");
+                    _npc.Agent.isStopped = false;
+                    _animator.SetBool(Talking, false);
+                    _npc.RemoveBehaviour(this);
+                }
+
+                yield return null;
+            }
+        }
+    }
+}
---
+++ AI Covid 19/Assets/_Scripts/AIBehaviour/IBehaviour/MeetingSystem.cs
@@ -0,0 +1,40 @@
+﻿using Covid19.Utils;
+using UnityEngine;
+
+namespace Covid19.AIBehaviour.Behaviour
+{
+    public class MeetingSystem
+    {
+        private AgentNPC _ownerNPC;
+        private Transform _transform;
+
+        public MeetingSystem(AgentNPC ownerOwnerNPC)
+        {
+            _ownerNPC = ownerOwnerNPC;
+            _transform = _ownerNPC.transform;
+        }
+        public AgentNPC FindNPCToMeet()
+        {
+            foreach (AgentNPC partnerNPC in _ownerNPC.ListAgents)
+            {
+                if (_ownerNPC != partnerNPC &&
+                    AIUtils.CanSeeObject(_transform, partnerNPC.transform, _ownerNPC.viewDist, _ownerNPC.viewAngle) &&
+                    partnerNPC.AcceptsMeeting(_ownerNPC))
+                {
+                    return partnerNPC;
+                }
+            }
+            return null;
+        }
+
+        public Vector3 GetMeetingPosition(AgentNPC npc)
+        {
+            var position = _transform.position;
+            Vector3 direction = npc.transform.position - position;
+            direction /= 2;
+            Vector3 meetingPosition = position + direction - _ownerNPC.offsetMeeting * direction.normalized;
+            
+            return meetingPosition;
+        }
+    }
+}
