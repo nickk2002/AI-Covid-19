a1abcca09229250177595f132ec9aeb4a0836872
nickk2002 <nicolae.filat@yahoo.com>
2020-07-15 21:55:55 +0300

    Removed files from git and added only the scripts

---
+++ AI Covid 19/Assets/_Scripts/AIBehaviour/IBehaviour/Configuration/PatrolConfiguration.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Covid19.AIBehaviour.Behaviour.States
+{
+    [CreateAssetMenu(menuName = "AI Behaviour/Patrol")]
+    public class PatrolConfiguration : ScriptableObject
+    {
+        public bool randomLocations = false; // if he uses randomLocations
+        public float randomRange = 10f; // the range of the random
+        public float stoppingDistance = 0.2f; // the distance at which the agent stops and moves to the next position
+
+    }
+}
