4459ba31ffc6b5657bc6ae1a34735515de0e0cbe
nickk2002 <nicolae.filat@yahoo.com>
2020-08-03 15:26:19 +0300

    Rearanged members in AgentConfiguration.cs trying

--- AI Covid 19/Assets/_Scripts/AI/Behaviour/Configuration/AgentConfiguration.cs
+++ AI Covid 19/Assets/_Scripts/AI/Behaviour/Configuration/AgentConfiguration.cs
@@ -6,20 +6,20 @@ namespace Covid19.AI.Behaviour.Configuration
     [CreateAssetMenu(menuName = "Covid19/AI/Data for Agent")]
     public class AgentConfiguration : ScriptableObject
     {
-        public int age = 35;
+        [Header("Debug")] public bool consoleDebug;
+        public bool sightDebug;
+        public bool drawRealSight;
 
         public AgentType agentType;
-
-        [Range(1, 10)] public int cautiousLevel = 5;
-        [Header("Debug")] public bool consoleDebug;
-
-        public float cooldownMeeting = 5;
-        public bool drawRealSight;
-        [Range(1, 10)] public int immunityLevel = 5;
         public string npcName = "Bob";
         public string occupation = "Bussines Man";
-        public bool sightDebug;
+        public int age = 35;
+
+        [Range(1, 10)] public int cautiousLevel = 5;
+        [Range(1, 10)] public int immunityLevel = 5;
         [Range(1, 10)] public int sociableLevel = 5;
+
+        public float cooldownMeeting = 5;
         [Range(0.1f, 4f)] public float stoppingDistance = 0.1f;
     }
 }
